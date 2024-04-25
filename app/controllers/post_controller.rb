2c91b06cada168c20e16e5de2340676b0ccc3407
denryuuu <denryuyellow@gmail.com>
2024-04-25 14:04:19 +0900

    ホーム画面

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: '投稿が保存されました'
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
