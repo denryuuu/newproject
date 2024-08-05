baf716f0312c6aa66fc52002f830c1fdae2be1dd
denryuuu <denryuyellow@gmail.com>
2024-08-06 03:50:11 +0900

    プロフィール編集画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_185253) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_182019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_185253) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar"
+    t.string "location"
+    t.string "favorite"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
