a197b32b85037cb8772737d9163b1573079c540d
denryuuu <denryuyellow@gmail.com>
2024-08-20 02:31:23 +0900

    削除できない状態

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,7 @@
 // Entry point for the build script in your package.json
-//= require rails-ujs
-//= require turbolinks
 //= require_tree .
+//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-Turbo.session.drive = false;
-document.addEventListener("turbolinks:load", function() {
-    Rails.start();
-});
+Turbo.session.drive = true;
