9388e413bbb2026c2a35718ee5045b19efcb64af
denryuuu <denryuyellow@gmail.com>
2024-09-09 13:05:27 +0900

    検索機能エラー中

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Newproject
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
