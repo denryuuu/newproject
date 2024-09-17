cbc12dc8cead8d911391d71e7cf3fc2f43b49fec
denryuuu <denryuyellow@gmail.com>
2024-09-17 13:52:19 +0900

    検索範囲の変更

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[title content] # titleとcontentが検索可能か確認
+    %w[title content created_at updated_at location_name address user_id] # titleとcontentが検索可能か確認
   end
 
   def self.ransackable_associations(auth_object = nil)
