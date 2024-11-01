8d81f6eb04b8ca1802e0ec6de7088936dc5dcb9d
denryuuu <denryuyellow@gmail.com>
2024-11-01 23:39:42 +0900

    いいね機能実装、場所名称

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,23 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 
   def own?(object)
     id == object&.user_id
   end
+
+  def like(post)
+    like_posts << post unless like?(post)
+  end
+  
+  def unlike(post)
+    like_posts.destroy(post)
+  end
+  
+  def like?(post)
+    like_posts.include?(post)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_190138) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_31_061122) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +54,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_190138) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -82,4 +94,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_190138) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
