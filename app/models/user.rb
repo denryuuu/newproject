abd240368b0cc9de9af9a21328c2c27dfc534c50
denryuuu <denryuyellow@gmail.com>
2025-03-30 12:49:16 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,17 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   mount_uploader :avatar, AvatarUploader
   has_many :posts, dependent: :destroy
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "無効なメールアドレスです" }
   validates :reset_password_token, uniqueness: true, allow_nil: true
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  validates :avatar, file_content_type: { allow: ['image/jpeg', 'image/png'] }
 
   def own?(object)
     id == object&.user_id
