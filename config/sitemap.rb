d0df427d737afc2d94689b49911434d70372a6c2
denryuuu <denryuyellow@gmail.com>
2025-04-01 00:51:56 +0900

    サイトマップ修正

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,4 @@
-SitemapGenerator::Sitemap.default_host = "https://your-app.herokuapp.com"
+SitemapGenerator::Sitemap.default_host = "https://ebc-sanctuary-map-7d9303cf86fb.herokuapp.com/"
 SitemapGenerator::Sitemap.create do
   add root_path, changefreq: 'daily', priority: 1.0
   add posts_path, changefreq: 'weekly', priority: 0.8
