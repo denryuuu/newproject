5b2a397ff9f8ad79293bd93cdac4223438bf29b8
denryuuu <denryuyellow@gmail.com>
2025-04-03 21:52:09 +0900

    version4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave'
 
 CarrierWave.configure do |config|
   config.storage = :cloudinary
-  config.cloudinary_credentials = {
+  config.cloudinary = {
     cloud_name:  ENV['CLOUDINARY_CLOUD_NAME'],
     api_key:     ENV['CLOUDINARY_API_KEY'],
     api_secret:  ENV['CLOUDINARY_API_SECRET']
